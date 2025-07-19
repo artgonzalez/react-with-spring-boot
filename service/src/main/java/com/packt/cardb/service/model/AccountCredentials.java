3985c8032304f7163fb7a026285398895555ee34
artgonzalezacc <arturo.gonzalez@austincc.edu>
2025-07-19 15:40:57 -0500

    configure jwt token

---
+++ service/src/main/java/com/packt/cardb/service/model/AccountCredentials.java
@@ -0,0 +1,13 @@
+package com.packt.cardb.service.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AccountCredentials {
+    private String username;
+    private String password;
+}
