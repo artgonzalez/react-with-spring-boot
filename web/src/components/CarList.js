8b99367b3f9147fa43f948787b57646a25c21456
artgonzalezacc <arturo.gonzalez@austincc.edu>
2025-07-20 23:30:40 -0500

    car delete

--- web/src/components/CarList.js
+++ web/src/components/CarList.js
@@ -3,31 +3,74 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { SERVER_URL } from "../constants";
 import { DataGrid } from "@mui/x-data-grid";
+import { Snackbar } from "@mui/material";
 
 export default function CarList() {
     const[cars, setCars] = useState([]);
+    const[open, setOpen] = useState(false);
+    const[errorMsg, setErrorMsg] = useState('');
+    
     const columns = [
         {field: 'brand', headerName: 'Brand', width: 200},
         {field: 'model', headerName: 'Model', width: 200},
         {field: 'color', headerName: 'Color', width: 200},
         {field: 'year', headerName: 'Year', width: 150},
         {field: 'price', headerName: 'Price', width: 150},
+        {
+            field: '_links.self.href',
+            headerName: '',
+            sortable: false,
+            filterable: false,
+            renderCell: row =>
+
+            <button
+                onClick={() => onDelClick(row.id)}>Delete
+            </button>
+            }
     ];
 
     useEffect(() => {
-        fetch(SERVER_URL + 'api/cars')
-        .then(response => response.json())
-        .then(data => setCars(data._embedded.cars))
-        .catch(err => console.error(err));        
+        fetchCars()
 
     }, []);
 
-    return (
+    const fetchCars = () => {
+        fetch(SERVER_URL + 'api/cars')
+        .then(response => response.json())
+        .then(data => setCars(data._embedded.cars))
+        .catch(err => console.error(err));
+    }
+
+    const onDelClick = (url) => {
+        if (window.confirm("Are you sure to delete?")) {
+            fetch(url,  {method:  'DELETE'})
+            .then(response => {
+                if( response.ok) {
+                    fetchCars();
+                    setErrorMsg('Car Deleted');
+                    setOpen(true);
+                } 
+                else {
+                    setErrorMsg('Car Delete Failed');
+                    setOpen(true);
+                }
+            })
+            .catch(err => console.error(err))
+        }    fetch(url,  {method:  'DELETE'})
+    }
+
+    return (        
         <div style={{ height: 500, width: '100%' }}>
             <DataGrid
-            rows={cars}
-            columns={columns}
-            getRowId={row => row._links.self.href}/>
+                rows={cars}
+                columns={columns}
+                getRowId={row => row._links.self.href}/>
+            <Snackbar
+                open={open}
+                autoHideDuration={2000}
+                onClose={() => setOpen(false)}
+                message={errorMsg}
+            />    
         </div>
     );
 }
